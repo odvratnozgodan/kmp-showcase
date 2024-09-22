90352cb6e35dd1fcbcc07b2fb198a7366b8a9a2c
Bruno Blazinc <odvratno.zgodan@gmail.com>
2024-09-22 17:13:00 +0200

    Simplify navigation routes

--- core/common/src/commonMain/kotlin/core/common/base/navigation/ScreenNavigationRoute.kt
+++ core/common/src/commonMain/kotlin/core/common/base/navigation/ScreenNavigationRoute.kt
@@ -4,23 +4,19 @@ import kotlinx.serialization.Serializable
 
 /**
  * Enum class used to represent the navigation route.
- *
- * @param name The path for the deep link
- * @arguments The arguments that can be provided(optional).
  */
-// TODO: Try to simplify deeplink
 @Serializable
-sealed class ScreenNavigationRoute(val name: String, val requiresLoggedInUser: Boolean = false) {
+sealed class ScreenNavigationRoute {
 
     @Serializable
-    data object Home : ScreenNavigationRoute("home", requiresLoggedInUser = true)
+    data object Home : ScreenNavigationRoute()
 
     @Serializable
-    data class RecipeDetails(val id: Int) : ScreenNavigationRoute("recipe/{id}", requiresLoggedInUser = true)
+    data class RecipeDetails(val id: Int) : ScreenNavigationRoute()
 
     @Serializable
-    data class Login(val sessionExpired: Boolean = false) : ScreenNavigationRoute("login")
+    data class Login(val sessionExpired: Boolean = false) : ScreenNavigationRoute()
 
     @Serializable
-    data object NoAction : ScreenNavigationRoute("")
+    data object NoAction : ScreenNavigationRoute()
 }
