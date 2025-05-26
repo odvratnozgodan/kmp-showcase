a14423badef227c35e4c983bc37cce58b04bce97
Bruno Blazinc <odvratno.zgodan@gmail.com>
2025-05-26 12:57:43 +0200

    Updated: Gradle wrapper, AGP, Kotlin, Compose, COMPILE_SDK_VERSION

--- build-logic/convention/src/main/kotlin/Versions.kt
+++ build-logic/convention/src/main/kotlin/Versions.kt
@@ -3,14 +3,14 @@ import org.gradle.api.JavaVersion
 object AppVersions {
     const val APP_ID = "com.kmp.showcase"
 
-    const val APP_VERSION_NAME = "1.0.4"
-    const val APP_VERSION_CODE = 104
+    const val APP_VERSION_NAME = "1.0.5"
+    const val APP_VERSION_CODE = 105
 }
 
 object Configs {
-    const val COMPILE_SDK_VERSION = 34
+    const val COMPILE_SDK_VERSION = 35
     const val MIN_SDK_VERSION = 26
-    const val TARGET_SDK_VERSION = 34
+    const val TARGET_SDK_VERSION = 35
     val JAVA_COMPATIBILITY_VERSION = JavaVersion.VERSION_11
 }
 
